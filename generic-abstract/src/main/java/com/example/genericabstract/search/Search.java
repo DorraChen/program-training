e1ff832c8665f566cdb30201a1daccbf2bfe8bc4
chenlizhen <1814786186@qq.com>
2020-07-07 14:13:19 +0800

    add readme.md

--- generic-abstract/src/main/java/com/example/genericabstract/search/Search.java
+++ generic-abstract/src/main/java/com/example/genericabstract/search/Search.java
@@ -4,6 +4,7 @@ package com.example.genericabstract.search;
  * @author clz
  * @data 2020/7/7 10:52
  * @description  编程训练: 泛型函数(数据类型的抽象, 数据结构的抽象)
+ * 在容器中搜索, 可以是链表,可以是map,可以是数组, 写出一个容器无关,类型无关的search函数
  */
 public class Search {
 
