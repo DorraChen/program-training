5bb11ef7be3a361dc89615dc641592bfdd186818
chenlizhen <1814786186@qq.com>
2020-09-22 14:48:44 +0800

    函数式编程, 三道编程题

--- generic-abstract/src/main/java/com/example/function/secondquestion/v3/AddMultiParams.java
+++ generic-abstract/src/main/java/com/example/function/secondquestion/v3/AddMultiParams.java
@@ -8,25 +8,10 @@ import java.util.function.Function;
  * @description 利用函数式编程, 将多个参数优化成单个参数
  */
 public class AddMultiParams {
-    public static Function<Function<Integer, Integer>, Function<Function<Integer, Integer>, Function<Integer, Integer>>>
-            compose1 = x -> y -> z -> x.apply(y.apply(z));
-//    public static Function<Function<Integer, Function<Integer, Function<Integer, Integer>>>, Function<Integer, Function<Integer, Integer>>>
-//            compose = x -> y -> z -> ;
 
     public static Function<Integer, Function<Integer, Integer>> add = x -> y -> x + y;
     public static Function<Integer, Function<Integer, Function<Integer, Integer>>> add1
             = x -> y ->z -> add.apply(x).apply(add.apply(y).apply(z));
-
-
-
-    /*public static BiFunction<BiFunction, Integer, Integer> addParam
-            = (f, x) -> (x == 0) ? 1 : x + (Integer) f.apply(f,x-1);
-
-    public static Function<Integer, Function<Integer, Integer>> addMultiParams =
-            x -> y -> addParam.apply(addParam, x);*/
-
-
-
 
     /**
      * 测试一下
@@ -34,7 +19,7 @@ public class AddMultiParams {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println(add1.apply(1).apply(2));
+        System.out.println(add1.apply(1).apply(2).apply(3));
     }
 
 }
