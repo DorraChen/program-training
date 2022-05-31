1c91ae0e6ccf94ec3116e8170a9034043d11d7f9
dorra <1814786186@qq.com>
2022-05-31 14:52:10 +0800

    group by multiple conditions

--- java8-learning/src/main/java/com/example/groupby/productsale/ProductSalesGroupByTest.java
+++ java8-learning/src/main/java/com/example/groupby/productsale/ProductSalesGroupByTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -24,6 +25,7 @@ public class ProductSalesGroupByTest {
         list.add(new ProductSalesDTO("1234", "3301", "2022-03-29", new BigDecimal("100")));
         list.add(new ProductSalesDTO("1234", "3303", "2022-03-29", new BigDecimal("100")));
         list.add(new ProductSalesDTO("2345", "3303", "2022-03-29", new BigDecimal("100")));
+        list.add(new ProductSalesDTO("2345", "3303", "2022-03-29", new BigDecimal("11.11")));
 
         Function<ProductSalesDTO, ProductSalesDTO> groupBy = ps ->
                 new ProductSalesDTO(ps.getProductId(), ps.getCityCode(), ps.getCreateTime());
@@ -33,6 +35,18 @@ public class ProductSalesGroupByTest {
                         Collectors.reducing(BigDecimal.ZERO, ProductSalesDTO::getOrderArea, BigDecimal::add)));
         map.forEach((k, v) -> log.info("productId={},cityCode={},creatTime={},v={}",
                 k.getProductId(), k.getCityCode(), k.getCreateTime(), v));
+        log.info("==========================");
+        map.forEach((k, v) -> log.info("k={},v={}", join(k.getProductId(), k.getCityCode(), k.getCreateTime()), v));
 
+        log.info("==========================");
+
+        Function<ProductSalesDTO, String> groupBy2 = ps -> join(ps.getProductId(), ps.getCityCode(), ps.getCreateTime());
+        Map<String, BigDecimal> map2 = list.stream().collect(Collectors.groupingBy(groupBy2, Collectors.reducing(BigDecimal.ZERO, ProductSalesDTO::getOrderArea, BigDecimal::add)));
+        map2.forEach((k, v) -> log.info("k={}, v={}", k, v));
+
+    }
+
+    private static String join(String... strings) {
+        return Arrays.stream(strings).collect(Collectors.joining("#", "", ""));
     }
 }
